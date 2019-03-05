f8371809ac721116a8a3d24c4e305737054ef624
wulman16 <will.ulman@gmail.com>
2019-03-05 15:55:57 -0500

    Flesh out users_controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+  before_action :require_login
+  skip_before_action :require_login, only: [:new, :create]
+
   def new
     @user = User.new
   end
@@ -7,17 +10,25 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to workouts_path
+      redirect_to user_path(@user)
     else
       flash[:error] = "We couldn't save your user."
       render :new
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:username, :password)
+  end
+
+  def require_login
+    return head(:forbidden) unless session[:user_id] == params[:id].to_i
   end
 
 end

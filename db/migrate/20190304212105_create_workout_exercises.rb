a1da3870d6333fb7aff7fedcf8cc6b7cd21d94f5
Megan Fago <meganlaynefago@gmail.com>
2019-03-05 12:41:06 -0500

    Created MVC, migrations, and seeds

---
+++ db/migrate/20190304212105_create_workout_exercises.rb
@@ -0,0 +1,14 @@
+class CreateWorkoutExercises < ActiveRecord::Migration[5.2]
+  def change
+    create_table :workout_exercises do |t|
+      t.integer :workout_id
+      t.integer :exercise_id
+      t.integer :sets
+      t.integer :reps
+      t.string :weight
+
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Workout.destroy_all
+Exercise.destroy_all
+WorkoutExercise.destroy_all
+
+will = User.create(name: "Will")
+megan = User.create(name: "Megan")
+
+upper_body_a = Workout.create(name: "Upper Body A", user_id: will.id)
+upper_body_b = Workout.create(name: "Upper Body B", user_id: megan.id)
+leg_day = Workout.create(name: "Leg Day", user_id: will.id)
+
+bench_press = Exercise.create(name: "Bench Press")
+curls = Exercise.create(name: "Curls")
+deadlifts = Exercise.create(name: "Deadlifts")
+
+WorkoutExercise.create(workout_id: upper_body_a.id, exercise_id: bench_press.id, sets: 4, reps: 12, weight: "100 lbs")
+WorkoutExercise.create(workout_id: upper_body_b.id, exercise_id: curls.id, sets: 3, reps: 25, weight: "30 lbs")
+WorkoutExercise.create(workout_id: leg_day.id, exercise_id: deadlifts.id, sets: 5, reps: 8, weight: "95 lbs")
